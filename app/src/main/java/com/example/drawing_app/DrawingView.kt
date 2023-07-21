506a76f72ce2663b0791187f9e32880f5ce19090
Oleh Donets <qyjodixilu@gmail.com>
2023-07-21 16:24:17 +0300

    code to keep drawing on screen

--- app/src/main/java/com/example/drawing_app/DrawingView.kt
+++ app/src/main/java/com/example/drawing_app/DrawingView.kt
@@ -14,6 +14,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize : Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas : Canvas? = null
+    private var mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -22,10 +23,10 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private fun setUpDrawing() {
         mDrawPaint = Paint()
         mDrawPath = CustomPath(color, mBrushSize)
-        mDrawPaint!!.color = color
-        mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mDrawPaint?.color = color
+        mDrawPaint?.style = Paint.Style.STROKE
+        mDrawPaint?.strokeJoin = Paint.Join.ROUND
+        mDrawPaint?.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
     }
@@ -39,6 +40,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for(path in mPaths) {
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+        }
+
         if(!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -71,6 +78,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 }
             }
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
