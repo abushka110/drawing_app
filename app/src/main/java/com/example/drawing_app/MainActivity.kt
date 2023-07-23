e0c50661c924e3956d5c5f5001161920ce663454
Oleh Donets <qyjodixilu@gmail.com>
2023-07-24 00:24:26 +0300

    brush button

--- app/src/main/java/com/example/drawing_app/MainActivity.kt
+++ app/src/main/java/com/example/drawing_app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.drawing_app
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,24 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val ib_brush= findViewById<ImageButton>(R.id.ib_brush)
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
